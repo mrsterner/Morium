0b5a992ee79da71f9d19ae97029ac2eb1da99bcc
mrsterner <simon@sterner.dev>
2022-01-20 12:50:56 +0100

    Texture and model tweaks

--- src/main/java/dev/mrsterner/alchrimea/client/renderer/DwarfInAFlaskItemRenderer.java
+++ src/main/java/dev/mrsterner/alchrimea/client/renderer/DwarfInAFlaskItemRenderer.java
@@ -1,11 +1,23 @@
 package dev.mrsterner.alchrimea.client.renderer;
 
 import dev.mrsterner.alchrimea.client.model.DwarfInAFlaskItemModel;
+import dev.mrsterner.alchrimea.client.renderlayer.AlchrimeaRenderLayer;
 import dev.mrsterner.alchrimea.common.block.AlchrimeaBlockItem;
+import dev.mrsterner.alchrimea.common.block.entity.EmeraldTabletBlockEntity;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
 
 public class DwarfInAFlaskItemRenderer extends GeoItemRenderer<AlchrimeaBlockItem> {
     public DwarfInAFlaskItemRenderer() {
         super(new DwarfInAFlaskItemModel());
+    }
+
+    @Override
+    public RenderLayer getRenderType(AlchrimeaBlockItem animatable, float partialTicks, MatrixStack stack, VertexConsumerProvider renderTypeBuffer, VertexConsumer vertexBuilder, int packedLightIn, Identifier textureLocation) {
+        return RenderLayer.getEntityTranslucent(getTextureLocation(animatable));
     }
 }
